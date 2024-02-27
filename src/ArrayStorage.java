b46c7f468942d622634d8d39aeea77c639715a0e
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-02-27 17:48:57 +0400

    try to do HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,53 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private static final int MAX_RESUMES = 10_000;
+    private final Resume[] storage = new Resume[MAX_RESUMES];
+    private int amountResumes;
 
     void clear() {
+        Arrays.fill(storage, 0, amountResumes, null);
+        amountResumes = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[amountResumes++] = resume;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < amountResumes; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                resume = storage[i];
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < amountResumes; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                amountResumes--;
+                if (i != amountResumes) {
+                    System.arraycopy(storage, i + 1, storage, i, amountResumes - i);
+                }
+                storage[amountResumes] = null;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, amountResumes);
     }
 
     int size() {
-        return 0;
+        return amountResumes;
     }
 }
