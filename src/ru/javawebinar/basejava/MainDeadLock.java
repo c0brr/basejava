c991ffc702457d561b29760120a56d9423c6eda4
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-06-25 14:27:18 +0400

    "HW_11_try2"

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -1,80 +1,52 @@
 package ru.javawebinar.basejava;
 
 public class MainDeadLock {
-    private static final SomeClass SOME_CLASS = new SomeClass();
-    private static final AnotherSomeClass ANOTHER_SOME_CLASS = new AnotherSomeClass();
+    private static final SomeClass SOME_CLASS_INSTANCE_1 = new SomeClass();
+    private static final SomeClass SOME_CLASS_INSTANCE_2 = new SomeClass();
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(() -> {
-            synchronized (ANOTHER_SOME_CLASS) {
-                ANOTHER_SOME_CLASS.incCounter();
-                try {
-                    ANOTHER_SOME_CLASS.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            synchronized (SOME_CLASS) {
-                SOME_CLASS.incCounter();
-                try {
-                    SOME_CLASS.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        Thread t2 = new Thread(() -> {
-            synchronized (ANOTHER_SOME_CLASS) {
-                ANOTHER_SOME_CLASS.incCounter();
-                try {
-                    ANOTHER_SOME_CLASS.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            synchronized (SOME_CLASS) {
-                SOME_CLASS.incCounter();
-                try {
-                    SOME_CLASS.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        t1.start();
-        t2.start();
-
+        Thread t1 = new Thread(() -> someAction(SOME_CLASS_INSTANCE_1, SOME_CLASS_INSTANCE_2));
+        Thread t2 = new Thread(() -> someAction(SOME_CLASS_INSTANCE_2, SOME_CLASS_INSTANCE_1));
+        startThreads(t1, t2);
         Thread.sleep(1000);
-
-        t1.join();
-        t2.join();
-
-        System.out.println(SOME_CLASS.getCounter() + ", " + ANOTHER_SOME_CLASS.getCounter());
-    }
-}
-
-class SomeClass {
-    private int counter;
-
-    public void incCounter() {
-        counter++;
+        joinThreads(t1, t2);
+        System.out.println(SOME_CLASS_INSTANCE_1.getCounter() + ", " + SOME_CLASS_INSTANCE_2.getCounter());
     }
 
-    public int getCounter() {
-        return counter;
-    }
-}
-
-class AnotherSomeClass {
-    private int counter;
-
-    public void incCounter() {
-        counter++;
+    private static void startThreads(Thread... threads) {
+        for (Thread t : threads) {
+            t.start();
+        }
     }
 
-    public int getCounter() {
-        return counter;
+    private static void joinThreads(Thread... threads) throws InterruptedException {
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+
+    private static void someAction(SomeClass someClass1, SomeClass someClass2) {
+        synchronized (someClass1) {
+            for (int i = 0; i < 1_000_000; i++) {
+                someClass1.incCounter();
+                if (i % 500_000 == 0) {
+                    synchronized (someClass2) {
+                        System.out.println("Hi!");
+                    }
+                }
+            }
+        }
+    }
+
+    private static class SomeClass {
+        private int counter;
+
+        public void incCounter() {
+            counter++;
+        }
+
+        public int getCounter() {
+            return counter;
+        }
     }
 }
