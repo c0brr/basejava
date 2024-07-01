21855d9312d33572847f2be2d5b3f4b2a22d9bdc
Ilya Erulaev <ilyaerulaev@gmail.com>
2024-07-01 22:10:25 +0400

    "HW_12_try2"

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -14,19 +14,15 @@ public class MainStreams {
         System.out.println(new MainStreams().oddOrEven(list));
     }
 
-    int minValue(int[] values) {
+    private int minValue(int[] values) {
         return Arrays.stream(values)
                 .distinct()
-                .boxed()
-                .sorted((num1, num2) -> num2 - num1)
-                .reduce((num1, num2) -> {
-                    num2 *= (int) Math.pow(10, (int) Math.log10(num1) + 1);
-                    return num1 + num2;
-                })
-                .get();
+                .sorted()
+                .reduce((subtotal, num) -> subtotal * 10 + num)
+                .getAsInt();
     }
 
-    List<Integer> oddOrEven(List<Integer> integers) {
+    private List<Integer> oddOrEven(List<Integer> integers) {
         return integers.stream()
                 .collect(Collectors.collectingAndThen(Collectors.partitioningBy(num -> num % 2 == 0),
                         map -> map.get(false).size() % 2 == 0 ? map.get(false) : map.get(true)));
